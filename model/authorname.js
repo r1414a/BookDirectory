b9e9ade78e16c47d3c686417faeb5639018851b4
r1414a <70930255+r1414a@users.noreply.github.com>
2023-02-10 09:28:06 +0530

    Update authorname.js

--- model/authorname.js
+++ model/authorname.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/BookDirectory', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/BookDirectory', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://r1414a:h4ck3r07@book-directory-cluster0.xfiw54k.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 //mongoose schema for adding authors
 
